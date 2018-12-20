261806898a0eae333c9a78e23a0112152d38dafa
Jorge Neto <jorgeneto@MacBook-Pro-de-Jorge.local>
2018-12-20 10:13:30 -0300

    reduce finalizado

--- 02-lists/for-forin/reduce.js
+++ 02-lists/for-forin/reduce.js
@@ -1,18 +1,33 @@
 const { obterPessoas } = require('./service')
 
-async function main(){
+Array.prototype.meuReduce = function (callback, valorInicial) {
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0]
+    for (let i = 0; i <= this.length-1; i++) {
+        valorFinal = callback(valorFinal, this[i], this)
+    }
+    return valorFinal
+}
+async function main() {
     try {
         const {
-            results  
+            results
         } = await obterPessoas('a')
 
         const pesos = results.map(item => parseInt(item.height))
-        const total = results.reduce((anterior,proximo)=>
-        {
-            return anterior + proximo
-        })
+        // const total = pesos.reduce((anterior, proximo) => {
+        //     return anterior + proximo
+        // })
+        const minhaLista = [
+            ['Jorge', 'Neto'],
+            ['Meu', 'Teste']
+        ]
 
-        console.log('total= ', total)
+        const total = minhaLista.meuReduce((anterior, proximo) => {
+            return anterior.concat(proximo)
+        }, [])
+        .join(', ')
+
+        console.log('total', total)
     } catch (error) {
         console.error('deu erro', error)
     }
